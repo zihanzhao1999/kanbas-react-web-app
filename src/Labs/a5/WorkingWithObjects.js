97b1da10209c40b2579a4337e9fe00d368a40cca
zihanzhao <zihanz@bu.edu>
2023-11-19 15:43:38 -0500

    change url

--- src/Labs/a5/WorkingWithObjects.js
+++ src/Labs/a5/WorkingWithObjects.js
@@ -9,7 +9,7 @@ function WorkingWithObjects() {
         completed: false,
         score: 0,
       });
-      const URL = "http://localhost:4000/a5/assignment";
+      const URL = "https://kanbas-node-server-app-stia.onrender.com/a5/assignment";
       const fetchAssignment = async () => {
         const response = await axios.get(`${URL}`);
         setAssignment(response.data);
@@ -54,13 +54,13 @@ function WorkingWithObjects() {
 
 
       <h4>Retrieving Objects</h4>
-      <a href="http://localhost:4000/a5/assignment"
+      <a href="https://kanbas-node-server-app-stia.onrender.com/a5/assignment"
          className="btn btn-primary me-2">
         Get Assignment
       </a>
       <h4>Retrieving Properties</h4>
       <a
-        href="http://localhost:4000/a5/assignment/title"
+        href="https://kanbas-node-server-app-stia.onrender.com/a5/assignment/title"
         className="btn btn-primary me-2">
         Get Title
       </a>
@@ -68,7 +68,7 @@ function WorkingWithObjects() {
 
       <h4>Score</h4>
       <a
-        href="http://localhost:4000/a5/assignment/score"
+        href="https://kanbas-node-server-app-stia.onrender.com/a5/assignment/score"
         className="btn btn-primary me-2">
         Get Score
       </a>
@@ -89,7 +89,7 @@ function WorkingWithObjects() {
 
 <h4>Completed</h4>
       <a
-        href="http://localhost:4000/a5/assignment/completed"
+        href="https://kanbas-node-server-app-stia.onrender.com/a5/assignment/completed"
         className="btn btn-primary me-2">
         Completion status
       </a>
